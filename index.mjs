04f9c62b88f1db5345df220e38646d8321e686c5
subhanrizwan <subhanahmed444sherazi@gmail.com>
2025-02-03 23:21:17 +0500

    changes

--- index.mjs
+++ index.mjs
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 // middleware
 app.use("/", (req, res, next) => {
