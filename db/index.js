71b8eeaabfbcdbc1bf59118873eed33586b8ff2c
subhanrizwan <subhanahmed444sherazi@gmail.com>
2025-02-21 13:02:58 +0500

    changes

---
+++ db/index.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb+srv://subhan:subhan@cluster0.s8chf.mongodb.net/practice?retryWrites=true&w=majority&appName=Cluster0");
+
+export default mongoose;
